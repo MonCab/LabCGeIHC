36d2e02c833fe2d3d8a0c3d9d93bb1d97bcb5932
MonCab <monoju_montse15@hotmail.com>
2019-04-12 12:54:34 -0500

    Práctica 9

--- Practica9/18-Skybox/src/main.cpp
+++ Practica9/18-Skybox/src/main.cpp
@@ -31,7 +31,9 @@
 std::shared_ptr<FirstPersonCamera> camera(new FirstPersonCamera());
 
 Sphere sphere(20, 20);
+Sphere sphere2(20, 20);
 Cylinder cylinder(20, 20, 0.5, 0.5);
+Cylinder cylinder2(20, 20, 0.5, 0.5);
 Box box;
 
 Shader shaderColor;
@@ -55,12 +57,12 @@ GLenum types[6] = {
 };
 
 std::string fileNames[6] = { 
-	"../../Textures/mp_bloodvalley/blood-valley_ft.tga",
-	"../../Textures/mp_bloodvalley/blood-valley_bk.tga",
-	"../../Textures/mp_bloodvalley/blood-valley_up.tga",
-	"../../Textures/mp_bloodvalley/blood-valley_dn.tga",
-	"../../Textures/mp_bloodvalley/blood-valley_rt.tga",
-	"../../Textures/mp_bloodvalley/blood-valley_lf.tga"
+	"../../Textures/mp_met/met_ft.tga",
+	"../../Textures/mp_met/met_bk.tga",
+	"../../Textures/mp_met/met_up.tga",
+	"../../Textures/mp_met/met_dn.tga",
+	"../../Textures/mp_met/met_rt.tga",
+	"../../Textures/mp_met/met_lf.tga"
 };
 
 int screenWidth;
@@ -145,7 +147,9 @@ void init(int width, int height, std::string strTitle, bool bFullScreen) {
 	shaderSpotLight.initialize("../../Shaders/typeLight.vs", "../../Shaders/spotLight.fs");
 
 	sphere.init();
+	sphere2.init();
 	cylinder.init();
+	cylinder2.init();
 	box.init();
 
 	camera->setPosition(glm::vec3(0.0f, 0.0f, 0.4f));
@@ -320,7 +324,7 @@ void applicationLoop() {
 
 	glm::vec3 objPosition = glm::vec3(0.0f, 0.0f, -3.0f);
 	float angle = 0.0;
-	float ratio = 5.0;
+	float ratio = 20.0;
 
 	while (psi) {
 		psi = processInput(true);
@@ -340,6 +344,7 @@ void applicationLoop() {
 		cylinder.setProjectionMatrix(projection);
 		cylinder.setViewMatrix(view);
 		cylinder.setShader(&shaderTexture);
+		cylinder.setPosition(glm::vec3(0.0, 0.0, -0.5));
 		cylinder.render(0, cylinder.getSlices() * cylinder.getStacks() * 2 * 3, cubeModelMatrix);
 		glBindTexture(GL_TEXTURE_2D, textureID1);
 		cylinder.render(cylinder.getSlices() * cylinder.getStacks() * 2 * 3, cylinder.getSlices() * 3, cubeModelMatrix);
@@ -348,34 +353,41 @@ void applicationLoop() {
 		glBindTexture(GL_TEXTURE_2D, 0);
 
 		glBindTexture(GL_TEXTURE_2D, textureID2);
-		cylinder.setShader(&shaderMateriales);
-		cylinder.setProjectionMatrix(projection);
-		cylinder.setViewMatrix(view);
-		cylinder.setPosition(glm::vec3(0.0, 0.0, 0.0));
-		cylinder.setScale(glm::vec3(1.0, 1.0, 1.0));
+		cylinder2.setShader(&shaderSpotLight);
+		cylinder2.setProjectionMatrix(projection);
+		cylinder2.setViewMatrix(view);
+		sphere2.setShader(&shaderSpotLight);
+		sphere2.setProjectionMatrix(projection);
+		sphere2.setViewMatrix(view);
+		sphere2.setPosition(glm::vec3(0.0, 0.0, 0.0));
+		sphere2.setScale(glm::vec3(1.0, 1.0, 1.0));
 		// Iluminación
 		glm::mat4 lightModelmatrix = glm::rotate(cubeModelMatrix, angle, glm::vec3(0.0f, 1.0f, 0.0f));
 		lightModelmatrix = glm::translate(lightModelmatrix, glm::vec3(0.0f, 0.0f, -ratio));
-		shaderMateriales.turnOn();
-		//glUniform3fv(shaderMateriales.getUniformLocation("light.position"), 1, glm::value_ptr(glm::vec3(lightModelmatrix * glm::vec4(0.0f, 0.0f, 0.0f, 1.0f))));
-		//glUniform3fv(shaderMateriales.getUniformLocation("light.direction"), 1, glm::value_ptr(glm::vec3(-0.3f, -1.0f, -0.2f)));
-		glUniform3fv(shaderMateriales.getUniformLocation("light.position"), 1, glm::value_ptr(camera->getPosition()));
-		glUniform3fv(shaderMateriales.getUniformLocation("light.direction"),1, glm::value_ptr(camera->getFront()));
-		glUniform3fv(shaderMateriales.getUniformLocation("viewPos"), 1, glm::value_ptr(camera->getPosition()));
-		glUniform3f(shaderMateriales.getUniformLocation("light.ambient"), 0.2, 0.2, 0.2);
-		glUniform3f(shaderMateriales.getUniformLocation("light.diffuse"), 0.2, 0.3, 0.6);
-		glUniform3f(shaderMateriales.getUniformLocation("light.specular"), 0.5, 0.3, 0.2);
-		glUniform3f(shaderMateriales.getUniformLocation("material.ambient"), 1.0, 0.2, 0.6);
-		glUniform3f(shaderMateriales.getUniformLocation("material.diffuse"), 0.4, 0.5, 0.8);
-		glUniform3f(shaderMateriales.getUniformLocation("material.specular"), 0.5, 0.3, 0.2);
-		glUniform1f(shaderMateriales.getUniformLocation("light.cutOff"), glm::cos(glm::radians(12.5f)));
-		glUniform1f(shaderMateriales.getUniformLocation("light.outerCutOff"), glm::cos(glm::radians(15.0f)));
-		glUniform1f(shaderMateriales.getUniformLocation("light.constant"), 1.0f);
-		glUniform1f(shaderMateriales.getUniformLocation("light.linear"), 0.14f);
-		glUniform1f(shaderMateriales.getUniformLocation("light.quadratics"), 0.07f);
-		glUniform1f(shaderMateriales.getUniformLocation("material.shininess"), 32.0);
-		cylinder.render();
-		shaderMateriales.turnOff();
+		shaderSpotLight.turnOn();
+		//glUniform3fv(shaderSpotLight.getUniformLocation("light.position"), 1, glm::value_ptr(glm::vec3(2.0, 0.0, 5.0)));//5.0
+		//glUniform3fv(shaderSpotLight.getUniformLocation("light.direction"), 1, glm::value_ptr(glm::vec3(-0.3f, -1.0f, -0.2f)));
+		glUniform3fv(shaderSpotLight.getUniformLocation("light.position"), 1, glm::value_ptr(camera->getPosition()));
+		glUniform3fv(shaderSpotLight.getUniformLocation("light.direction"),1, glm::value_ptr(camera->getFront()));
+		glUniform3fv(shaderSpotLight.getUniformLocation("viewPos"), 1, glm::value_ptr(camera->getPosition()));
+		glUniform3f(shaderSpotLight.getUniformLocation("light.ambient"), 0.2, 0.2, 0.2);
+		glUniform3f(shaderSpotLight.getUniformLocation("light.diffuse"), 0.2, 0.3, 0.6);
+		glUniform3f(shaderSpotLight.getUniformLocation("light.specular"), 0.5, 0.3, 0.2);
+		glUniform3f(shaderSpotLight.getUniformLocation("material.ambient"), 1.0, 0.2, 0.6);
+		glUniform3f(shaderSpotLight.getUniformLocation("material.diffuse"), 0.4, 0.5, 0.8);
+		glUniform3f(shaderSpotLight.getUniformLocation("material.specular"), 0.5, 0.3, 0.2);
+		glUniform1f(shaderSpotLight.getUniformLocation("light.cutOff"), glm::cos(glm::radians(5.0f)));
+		glUniform1f(shaderSpotLight.getUniformLocation("light.outerCutOff"), glm::cos(glm::radians(7.0f)));
+		glUniform1f(shaderSpotLight.getUniformLocation("light.constant"), 1.0f);
+		glUniform1f(shaderSpotLight.getUniformLocation("light.linear"), 0.7f);
+		glUniform1f(shaderSpotLight.getUniformLocation("light.quadratics"), 1.8f);
+		glUniform1f(shaderSpotLight.getUniformLocation("material.shininess"), 32.0);
+		sphere2.render();
+		sphere2.setPosition(glm::vec3(0.0, 0.0, -20.0));
+		sphere2.render();
+		cylinder2.setPosition(glm::vec3(0.0, 0.0, -12.0));
+		cylinder2.render();
+		shaderSpotLight.turnOff();
 		glBindTexture(GL_TEXTURE_2D, 0);
 
 		if (angle > 2 * M_PI)
